96fa5950d8f2cda7036c108800c698811aeea399
n9Mtq4 <bill.wresnahan@gmail.com>
2015-10-02 20:23:50 -0400

    shows full forge list

--- ForgeModSupportTab/src/main/java/com/n9mtq4/exmcl/tab/forgemods/ui/InstallForgeDialog.java
+++ ForgeModSupportTab/src/main/java/com/n9mtq4/exmcl/tab/forgemods/ui/InstallForgeDialog.java
@@ -1,9 +1,7 @@
 package com.n9mtq4.exmcl.tab.forgemods.ui;
 
 import com.n9mtq4.exmcl.tab.forgemods.html.Downloader;
-import com.n9mtq4.logwindow.utils.JarLoader;
 import com.n9mtq4.logwindow.utils.StringParser;
-import net.minecraftforge.installer.SimpleInstaller;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -19,6 +17,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Created by will on 7/28/15 at 10:58 PM.
@@ -30,8 +29,11 @@ public final class InstallForgeDialog {
 	* http://adfoc.us/serve/sitelinks/?id=271228&url=http://files.minecraftforge.net/maven/net/minecraftforge/forge/1.8-11.14.3.1499/forge-1.8-11.14.3.1499-installer.jar
 	* */
 	
+	private static final String MCVERSION_SELECTOR = "body > div.contents > div.wrapper > section:nth-child(1) > div.versions-wrapper > div > ul > li > div > ul > li > a";
+	private static final String LINK_SELECTOR = "#downloadsTable > tbody > tr > td:nth-child(3) > ul > li:nth-child(2) > a:nth-child(1)";
+	
 	private static final String FORGE_FILES_URL = "http://files.minecraftforge.net/";
-	private static final String URL_PREFIX = "http://adfoc.us/serve/sitelinks/?id=271228&url=";
+	private static final String URL_SPLIT = "&url=";
 	private static final String URL_FORMAT = "http://files.minecraftforge.net/maven/net/minecraftforge/forge/%s/forge-%s-installer.jar";
 	
 	private final ForgeTab forgeTab;
@@ -54,7 +56,7 @@ public final class InstallForgeDialog {
 		
 		frame = new JFrame("Select Forge Versions");
 		
-		table = new JTable(getListOfForges(), new Object[]{"MC Version", "Forge Version"});
+		table = new JTable(getListOfForges(), new Object[]{"MC Version", "Forge Version", "Url"});
 		table.getTableHeader().setReorderingAllowed(false);
 		scroll = new JScrollPane(table);
 		select = new JButton("Download and Install");
@@ -82,9 +84,24 @@ public final class InstallForgeDialog {
 	}
 	
 	/**
-	 * Runs the forge mod installer that was just downloaded
+	 * Tries to run the downloaded forge installer
+	 * should work with most versions
 	 * */
 	private void run(File file) {
+		
+		Runtime re = Runtime.getRuntime();
+		try{
+			re.exec("java -jar " + file.getAbsolutePath());
+		} catch (IOException ioe){
+			ioe.printStackTrace();
+		}
+		
+	}
+	
+	/*
+	* This only works with the latest 1.8 versions
+	* */
+/*	private void run(File file) {
 		try {
 			
 //			adds the jar so we can access it
@@ -107,7 +124,7 @@ public final class InstallForgeDialog {
 			JOptionPane.showMessageDialog(frame, "Error. Couldn't open the forge installer.", "Error", JOptionPane.ERROR_MESSAGE);
 		}
 		
-	}
+	}*/
 	
 	/**
 	 * Downloads the forge mod installer
@@ -115,9 +132,10 @@ public final class InstallForgeDialog {
 	private File download(final ActionEvent e) {
 		
 		int row = table.getSelectedRow();
-		String mcVersion = (String) table.getValueAt(row, 0);
+//		String mcVersion = (String) table.getValueAt(row, 0);
 		String forgeVersion = (String) table.getValueAt(row, 1);
-		String url = makeUrl(mcVersion, forgeVersion);
+//		String url = makeUrl(mcVersion, forgeVersion);
+		String url = (String) table.getValueAt(row, 2);
 		System.out.println(url);
 		
 		File tmpDir = new File("tmp/");
@@ -136,11 +154,71 @@ public final class InstallForgeDialog {
 		
 	}
 	
-	/**
-	 * Tries to download a list of forge versions
-	 * */
-//	TODO: full list with other minecraft versions
 	private static Object[][] getListOfForges() {
+		
+		ArrayList<Object[]> versions = new ArrayList<Object[]>();
+		
+		try {
+			
+			Document index = Jsoup.connect(FORGE_FILES_URL).get();
+			
+			Elements mcVersions = index.select(MCVERSION_SELECTOR);
+			
+			Elements linksIndex = index.select(LINK_SELECTOR);
+			
+			for (Element version : linksIndex) {
+				
+				String adLink = version.attr("href");
+				String link = adLink.split(URL_SPLIT)[1];
+				
+				StringParser parser = new StringParser(link);
+				String chop = parser.getBetween("/maven/net/minecraftforge/forge/", "-installer.jar");
+				chop = chop.split("/")[0];
+				String[] tokens = chop.split("-");
+				
+				versions.add(new Object[]{tokens[0], tokens[1], link});
+				
+			}
+			
+			for (Element mcVersion : mcVersions) {
+				
+				try {
+					Document doc = Jsoup.connect(mcVersion.attr("href")).get();
+					Elements links = doc.select(LINK_SELECTOR);
+					
+					for (Element version : links) {
+						
+						String adLink = version.attr("href");
+						String link = adLink.split(URL_SPLIT)[1];
+						
+						StringParser parser = new StringParser(link);
+						String chop = parser.getBetween("/maven/net/minecraftforge/forge/", "-installer.jar");
+						chop = chop.split("/")[0];
+						String[] tokens = chop.split("-");
+						
+						versions.add(new Object[]{tokens[0], tokens[1], link});
+						
+					}
+					
+				}catch (Exception e) {
+//					prevent a bad formatting on an older version to mess with things
+				}
+				
+			}
+			
+			Object[][] oVersions = new Object[versions.size()][3];
+			versions.toArray(oVersions);
+			return oVersions;
+			
+		}catch (Exception all) {
+			all.printStackTrace();
+			return new Object[][] {{"Error Downloading Forge List", "Please try again"}};
+		}
+		
+	}
+	
+//	TODO: full list with other minecraft versions
+/*	private static Object[][] getListOfForges() {
 		
 		try {
 			
@@ -164,6 +242,6 @@ public final class InstallForgeDialog {
 			return new Object[][] {{"Error Downloading Forge", "Error Downloading Forge"}};
 		}
 		
-	}
+	}*/
 	
 }
