94dd5054dfc06d6fafdaad9e5a9fc1a481a3584d
n9Mtq4 <bill.wresnahan@gmail.com>
2015-10-03 12:00:17 -0400

    deleted old code

--- ForgeModSupportTab/src/main/java/com/n9mtq4/exmcl/tab/forgemods/ui/InstallForgeDialog.java
+++ ForgeModSupportTab/src/main/java/com/n9mtq4/exmcl/tab/forgemods/ui/InstallForgeDialog.java
@@ -34,7 +34,6 @@ public final class InstallForgeDialog {
 	
 	private static final String FORGE_FILES_URL = "http://files.minecraftforge.net/";
 	private static final String URL_SPLIT = "&url=";
-	private static final String URL_FORMAT = "http://files.minecraftforge.net/maven/net/minecraftforge/forge/%s/forge-%s-installer.jar";
 	
 	private final ForgeTab forgeTab;
 	private JFrame frame;
@@ -100,34 +99,6 @@ public final class InstallForgeDialog {
 		
 	}
 	
-	/*
-	* This only works with the latest 1.8 versions
-	* */
-/*	private void run(File file) {
-		try {
-			
-//			adds the jar so we can access it
-			JarLoader.addFile(file);
-//			start the install main method
-			new Thread(new Runnable() {
-				@Override
-				public void run() {
-					try {
-						SimpleInstaller.main(new String[]{});
-					}catch (IOException e) {
-						e.printStackTrace();
-						JOptionPane.showMessageDialog(frame, "Error. Couldn't open the forge installer.", "Error", JOptionPane.ERROR_MESSAGE);
-					}
-				}
-			}).start();
-			
-		}catch (IOException e) {
-			e.printStackTrace();
-			JOptionPane.showMessageDialog(frame, "Error. Couldn't open the forge installer.", "Error", JOptionPane.ERROR_MESSAGE);
-		}
-		
-	}*/
-	
 	/**
 	 * Downloads the forge mod installer
 	 * */
@@ -146,13 +117,6 @@ public final class InstallForgeDialog {
 		File file = new File(tmpDir, "forge_" + forgeVersion + ".jar");
 		Downloader.downloadFile(url, file);
 		return file;
-		
-	}
-	
-	private static String makeUrl(String mcVersion, String forgeVersion) {
-		
-		String forgeId = mcVersion + "-" + forgeVersion;
-		return String.format(URL_FORMAT, forgeId, forgeId);
 		
 	}
 	
@@ -218,32 +182,5 @@ public final class InstallForgeDialog {
 		}
 		
 	}
-	
-//	TODO: full list with other minecraft versions
-/*	private static Object[][] getListOfForges() {
-		
-		try {
-			
-			Document doc = Jsoup.connect(FORGE_FILES_URL).get();
-			Elements links = doc.select(".link > a");
-			Element dlLine = links.get(6);
-			
-			String adLink = dlLine.attr("href");
-			String link = adLink.substring(URL_PREFIX.length());
-			
-//			using LogWindow's parsing for more advanced things
-			StringParser parser = new StringParser(link);
-			String chop = parser.getBetween("/maven/net/minecraftforge/forge/", "-installer.jar");
-			chop = chop.split("/")[0];
-			String[] tokens = chop.split("-");
-			
-			return new Object[][] {{tokens[0], tokens[1]}, {"There will be the full", "list of forge dls in a later version."}};
-			
-		}catch (IOException e) {
-			e.printStackTrace();
-			return new Object[][] {{"Error Downloading Forge", "Error Downloading Forge"}};
-		}
-		
-	}*/
 	
 }
